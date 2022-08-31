cdea1354a9e09d3b258605910de383b9bcb92457
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-08-31 21:50:27 +0600

    Update Body.js

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -2,22 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./body.scss";
 
 const Body = (props) => {
-	const { array } = props;
+	const { array, colors, barWidth } = props;
 	console.log("Updated");
-
-	const [colors, setColors] = useState([]);
-	const [barWidth, setBarWidth] = useState(0);
-
-	// generate random colors for array
-	useEffect(() => {
-		const colors = [];
-		for (let i = 0; i < array.length; i++) {
-			colors.push(`hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`);
-		}
-		setColors(colors);
-		const barWidth = 100 / array.length + "%";
-		setBarWidth(barWidth);
-	}, [array]);
 
 	return (
 		<div className="body">
